708028692af44a96fde6e594a2863206220795a0
Paulo <124407850+PauloSantos2002@users.noreply.github.com>
2025-04-27 23:32:35 -0300

    tratamento de bugs

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function CalculoDeIMC() {
     var peso = parseFloat(document.getElementById("peso").value);
     var altura = parseFloat(document.getElementById("altura").value);
     
-    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
-        document.getElementById("resultado").innerHTML = ("Por favor, insira valores válidos para peso e altura.");
+    if (isNaN(peso) || isNaN(altura) || imc <= 0 || altura > 2.50) {
+        document.getElementById("resumoIMC").innerHTML = ("Por favor, insira valores válidos para peso e altura.");
         return;
     }else{
         var imc = IMC(peso, altura);
@@ -17,4 +17,16 @@ function CalculoDeIMC() {
         document.getElementById("resultado").innerHTML = "Seu IMC é: " + imc.toFixed(2);
 
     }
-}
+
+    if (imc > 35) {
+        document.getElementById("resumoIMC").innerHTML = "Obesidade Extrema";
+    } else if(imc > 35){
+        document.getElementById("resumoIMC").innerHTML = "Obesidade";
+    } else if(imc > 25){
+        document.getElementById("resumoIMC").innerHTML = "exceso de peso";
+    } else if(imc > 18.5){
+        document.getElementById("resumoIMC").innerHTML = "peso normal";
+    } else {
+        document.getElementById("resumoIMC").innerHTML = "Baixo peso";
+    } 
+} 
